21fd1de85da11fce3775cea1f1e5e9079850c811
StarYaga <yagacorp7@gmail.com>
2025-10-30 17:16:01 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js — Mariachi Soundboard PWA
-const VERSION = "mariachi-v1";
+const VERSION = "mariachi-v3";
 const BASE = "/mariachi/"; // ¡Importante! Ruta base en GitHub Pages
 
 // Archivos que precacheamos para abrir offline
